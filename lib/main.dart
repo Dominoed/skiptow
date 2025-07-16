67d6c13d6b9eaddc4a8d0d27bdaaa1c3d581ad34
Dominoed <Brandon.Postlethwait311@gmail.com>
2025-07-16 15:33:44 -0400

    feat: auto redirect based on auth state

--- lib/main.dart
+++ lib/main.dart
@@ -1,23 +1,29 @@
 import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'firebase_options.dart';
 import 'pages/login_page.dart';
+import 'pages/dashboard_page.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
-  runApp(const MyApp());
+  final user = FirebaseAuth.instance.currentUser;
+  runApp(MyApp(initialUserId: user?.uid));
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+  final String? initialUserId;
+  const MyApp({super.key, this.initialUserId});
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'SkipTow',
       debugShowCheckedModeBanner: false,
       theme: ThemeData(colorScheme: ColorScheme.fromSeed(seedColor: Colors.orange)),
-      home: const LoginPage(),
+      home: initialUserId != null
+          ? DashboardPage(userId: initialUserId!)
+          : const LoginPage(),
     );
   }
 }
