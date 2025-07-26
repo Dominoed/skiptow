f8e22b527bded6164fb30c662ad78f089542575d
Dominoed <Brandon.Postlethwait311@gmail.com>
2025-07-26 13:35:39 -0400

    Lock orientation to portrait

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'dart:async';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_messaging/firebase_messaging.dart';
@@ -25,6 +26,10 @@ import 'deep_link_state.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
+  await SystemChrome.setPreferredOrientations([
+    DeviceOrientation.portraitUp,
+    DeviceOrientation.portraitDown,
+  ]);
   await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
   await _pushService.initialize(onNotificationTap: _handleNotificationTap);
   FirebaseMessaging.onBackgroundMessage(_firebaseMessagingBackgroundHandler);
